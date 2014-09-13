cc994d144deffabc18172bfa41dd3a79a512555b
RachelNolan <mail@rachelnolan.com>
2014-09-13 12:36:35 +0200

    Added interactive menu - refactored code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+@students = [] #an empty array accesible to all methods, an instance variable
+
 #Defining a method to print the header 
 def print_header
 	puts "The students of my cohort at Makers Academy"
@@ -5,44 +7,67 @@ def print_header
 end
 
 #Defining a method to print the student details 
-def print(students)
-	students.each do |student|
+def print_students_list
+	@students.each do |student|
 		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 
 #Defining a method to print the footer
-def print_footer(names)
-	puts "Overall, we have #{names.length} great students."
+def print_footer
+	puts "Overall, we have #{@students.length} great students."
 end
 
 #Asking the user for input 
 def input_students
 	puts "Please enter the names of the students"
 	puts "To finish, just hit return twice."
-
-	#Create empty array
-	students = []
 	#Get the first names
 	name = gets.chomp
 	#While the name is not empty, repeat this code. 
-
 	while !name.empty? do
 		#Add student hash to the array
-		students << {:name => name, :cohort => :October}
-		puts "Now we have #{students.length} students."
+		@students << {:name => name, :cohort => :October}
+		puts "Now we have #{@students.length} students."
 		#Get another name from user
 		name = gets.chomp
 	end
-	#return the array of students
-	students
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def print_menu
+	#print menu and ask user what to
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit"#9 beacuse we'll be adding more items
+end
 
+def show_students
+	print_header
+	print_students_list
+	print_footer
+end
 
+def process(selection)
+	case selection
+		when "1"
+			input_students
+		when "2"
+			show_students
+		when "9"
+			exit #this will cause program to terminate
+		else
+			puts "I don't know what you meant, try again"
+	end
+end
+
+#Interactive menu.
+def interactive_menu
+	loop do
+		print_menu
+		process(gets.chomp)
+	end
+end
+
+interactive_menu
 
 
