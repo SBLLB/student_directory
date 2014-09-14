5643b953afaad9a700361a3b2d18f139ff5889bc
RachelNolan <mail@rachelnolan.com>
2014-09-14 11:15:36 +0200

    Exercise 2, added index count

--- directory.rb
+++ directory.rb
@@ -6,8 +6,8 @@ end
 
 #Defining a method to print the student details 
 def print(students)
-	students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+	students.each_with_index do |student, index|
+		puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 
